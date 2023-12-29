03ac71f22dfb3ce84275e6f91c0371bf185e8ae8
zat <theytaedu03@gmail.com>
2023-12-29 13:51:34 +0100

    add dataschema, create class from json and create json from class

--- src/log_analyser/object/character.py
+++ src/log_analyser/object/character.py
@@ -4,12 +4,17 @@ from object import Object
 class Character(Object):
     
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    
+        
+        data_schema = {"name": str, 
+                       "age": int}
+        
+        super().__init__(data_schema, **kwargs)
     
     def __print__(self):
         print(vars(self))
     
-character = Character.from_json({"name": "ok", "test": "lol"})
+character = Character.from_json({"name": "ok", "age": 3})
 
 character.__print__()
+
+print(character.export_json())
--- src/log_analyser/object/object.py
+++ src/log_analyser/object/object.py
@@ -2,14 +2,31 @@ import json
 
 
 class Object:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, class_model, **kwargs):   
+        
+        self.from_json_ok = True
     
+        for key, value in kwargs.items():
+            if key in class_model:
+                if type(value) == class_model[key]:
+                    setattr(self, key, value)
+                else:
+                    print("type of key {} is invalid".format(key))
+                    self.from_json_ok = False
+            else:
+                print("key {} not find in dataschema".format(key))
+                self.from_json_ok = False
+                
     def export_json(self):
         
-        return json.dumps(self.__dict__)
+        dict_class = self.__dict__
+        dict_class.pop("from_json_ok")
+        return json.dumps(dict_class)
     
     @classmethod
     def from_json(self, data):
-        return self(**data)
+        object = self(**data)
+        if object.from_json_ok:
+            return object
+        else:
+            return None
